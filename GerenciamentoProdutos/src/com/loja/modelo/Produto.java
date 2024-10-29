a72c9d0a9f8ae0a87ad7e0eb3bb0a0cbf5513c55
TAINÁ ESTEFANI MARTINS <taina_e_martins@estudante.sesisenai.org.br>
2024-10-28 21:30:53 -0300

    feat: estrutura inicial do projeto com pacotes e implementação da classe Produto

---
+++ GerenciamentoProdutos/src/com/loja/modelo/Produto.java
@@ -0,0 +1,76 @@
+package com.loja.modelo;
+
+import java.util.Objects;
+
+public class Produto {
+    private Integer id;
+    private String nome;
+    private double preco;
+    private int quantidadeEstoque;
+    private String categoria;
+
+    public Produto(String nome, double preco, int quantidadeEstoque, String categoria) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Produto {ID: %d, Nome: '%s', Preço: %.2f, Quantidade em Estoque: %d, Categoria: '%s'}",
+                id, nome, preco, quantidadeEstoque, categoria);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto produto)) return false;
+        return getId().equals(produto.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
