66fcba80851877909e743ae02f41d1e7f6c86402
TAINÁ ESTEFANI MARTINS <taina_e_martins@estudante.sesisenai.org.br>
2024-10-28 21:42:29 -0300

    feat: adiciona classe GerenciadorProdutos

---
+++ GerenciamentoProdutos/src/com/loja/gerenciador/GerenciadorProdutos.java
@@ -0,0 +1,13 @@
+package com.loja.gerenciador;
+import com.loja.modelo.Produto;
+
+import java.util.List;
+
+public class GerenciadorProdutos {
+    private List<Produto> produtos;
+    private int proximoId;
+
+    public void criar(Produto produto) {
+        
+    }
+}
